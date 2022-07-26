c88b8a9257228487d2ec572ad4b1be99f03efaf9
tuncayofficial <qtuncay70@gmail.com>
2022-07-26 22:11:53 +0400

    First initialize

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+SECONDS=0
+secs=10
+clear
 echo Wi-Fi Connect proqramına xoş gəlmişsiniz! Proqramın bütün nüsxələri və hüquqları @tuncayofficial-a aiddir.
 sleep 2
 echo Wi-Fi Connect yüklənsin? Y/n
@@ -8,12 +13,10 @@ echo IWCTL yüklənir..
 
 chars="/-\|"
 
-while :; do
+while (( SECONDS < secs )); do
   for (( i=0; i<${#chars}; i++ )); do
     sleep 0.5
     echo -en "${chars:$i:1}" "\r"
   done
 done
-
-sleep 3.5
-echo IWCTL yükləndi! Komanda sətrinə IWCTL yazaraq proqramı başladın!
+echo IWCTL yükləndi! Komanda sətrinə iwctl yazaraq proqramı başladın!
